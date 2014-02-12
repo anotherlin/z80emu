2ca832d7214104e0443aadce80048b54a0b84398
Lin Ke-Fong <another_lin@yahoo.com>
2014-02-12 13:54:31 +0100

    Update macros.h
    Header update.

--- macros.h
+++ macros.h
@@ -1,7 +1,7 @@
 /* macros.h
  * Helper macros definitions.
  *
- * Copyright (c) 2012 Lin Ke-Fong
+ * Copyright (c) 2012-2014 Lin Ke-Fong
  *
  * This program is free, do whatever you want with it.
  */
