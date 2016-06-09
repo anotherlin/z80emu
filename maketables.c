8a956e8d9543a710271954ccfe02c7c3bb673280
Lin Ke-Fong <anotherlin@gmail.com>
2016-06-09 20:36:50 +0200

    z80emu-1.0.3

--- maketables.c
+++ maketables.c
@@ -84,7 +84,6 @@ static void make_instruction_table (void)
                                                 };
 
                                 s = strings[i];
-
                                 break;
 
                         }
@@ -116,7 +115,6 @@ static void make_instruction_table (void)
                                                 };
 
                                 s = strings[i];
-
                                 break;
 
                         }
@@ -190,7 +188,6 @@ static void make_instruction_table (void)
                                                 };
 
                                 s = strings[i];
-
                                 break;
 
                         }
@@ -368,7 +365,6 @@ static void make_instruction_table (void)
 
                                 printf("%s", accumulator_operations[i]);
                                 s = "_N";
-                                
                                 break;
 
                         }
